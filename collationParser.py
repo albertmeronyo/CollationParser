b69bee93d25406935ed6b0ba853954ecf2a34d4a
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2013-04-17 17:20:04 +0200

    Collation regexp development

--- collationParser.py
+++ collationParser.py
@@ -46,39 +46,50 @@ WHERE {
 """)
 
 sparql.setReturnFormat(JSON)
-#results = sparql.query().convert()
+results = sparql.query().convert()
 
-results = ["A-G`SUP`12`LO` H`SUP`6`LO` (lacks H6, blank?)", "`SUP`8`LO` (*1+Û1) A-2C`SUP`8`LO` 2D`SUP`4`LO`", "`SUP`8`LO` A-L`SUP`8`LO` M`SUP`6`LO`"]
+#results = ["A-G`SUP`12`LO` H`SUP`6`LO` (lacks H6, blank?)", "`SUP`8`LO` (*1+Û1) A-2C`SUP`8`LO` 2D`SUP`4`LO`", "`SUP`8`LO` A-L`SUP`8`LO` M`SUP`6`LO`"]
 
-#for r in results["results"]["bindings"]:
-for r in results:
-    #f = r["f"]["value"]
-    f = r
-    print "Formula:", f
+for r in results["results"]["bindings"]:
+#for r in results:
+    f = r["f"]["value"]
+    #f = r
+    print "Formula:", f.encode('utf8')
     prog = re.compile('''
-                      (\`SUP\`(?P<pren>[0-9]+)\`LO\`\s*(?P<pre>(\(.*\)\s*))?)?   # Pre-part
-                      (?P<start_sheet>[A-Z])(-(?P<end_sheet_n>[0-9]*)(?P<end_sheet>[A-Z]))?\`SUP\`(?P<sheet_count>[0-9]+)\`LO\`\s*((?P<remain>[0-9]*[A-Z])(\`SUP\`)?(?P<remain_n>[0-9]+)(\`LO\`)?)?.*'''
+                      (.(?P<cover_sheet>>[0-9]))?                                  # Cover sheet
+                      (\`SUP\`(?P<pren>[0-9]+)\`LO\`\s*(?P<pre>(\(.*\)\s*))?)?     # Preface sheets
+                      (?P<start_sheet>[A-Z])(-(?P<end_sheet_n>[0-9]*)(?P<end_sheet>[A-Z]))?\`SUP\`(?P<sheet_count>[0-9]+)(\/[0-9])?\`LO\`\s*                                                                     # Main block sheets
+                      ((?P<remain>[0-9]*[A-Z])\`SUP\`(?P<remain_n>[0-9]+)\`LO\`)?  # Remaining sheets
+                      .*                                                           # Trail
+                      '''
                       , re.VERBOSE)
     matches = prog.search(f)
     if matches:
+        if matches.group("cover_sheet"):
+            cover_sheet = int(matches.group("cover_sheet"))
+        else:
+            cover_sheet = 0
         start_sheet = matches.group("start_sheet")
         end_sheet = matches.group("end_sheet")
-        try:
+        if matches.group("end_sheet_n"):
             end_sheet_n = int(matches.group("end_sheet_n"))
-        except ValueError:
+        else:
             end_sheet_n = 1
         sheet_count = int(matches.group("sheet_count"))
         remain_sheet = matches.group("remain")
-        try:
+        if matches.group("remain_n"):
             remain_n = int(matches.group("remain_n"))
-        except ValueError:
+        else:
             remain_n = 0
-        except TypeError:
-            remain_n = 0
-        try:
+        if matches.group("pren"):
             pren = int(matches.group("pren"))
-        except TypeError:
+        else:
             pren = 0
+    else:
+        print "Regexp does not recognise this formula!"
+        print
+        continue
+    print start_sheet, end_sheet
     sheets = pren + ((end_sheet_n - 1) * len(g) + g[end_sheet] - g[start_sheet] + 1) * sheet_count + remain_n
     pages = sheets * 2
     #print pren, " + ((", end_sheet_n, "- 1 ) * ", len(g)," + ", g[end_sheet], " - ", g[start_sheet], " + 1) *", sheet_count, " + ", remain_n
@@ -86,5 +97,3 @@ for r in results:
     print "Pages:", pages
     print
     
-    
-        
